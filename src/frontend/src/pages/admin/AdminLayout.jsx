a337911e8385c1b53ac16914aa7ded43d7b00539
LLVB160 <huynhnguyenthienbao1509@gmail.com>
2025-10-17 16:38:13 +0700

    admin-bus,parent,student

--- src/frontend/src/pages/admin/AdminLayout.jsx
+++ src/frontend/src/pages/admin/AdminLayout.jsx
@@ -1,6 +1,9 @@
 import { FaUserCircle } from "react-icons/fa";
 import DriversPage from "./DriversPage";
 import { Link, Routes, Route, useLocation } from "react-router-dom";
+import ManageStudent from "./ManageStudent";
+import ManageBus from "./ManageBus";
+import ManageParent from "./ManageParent";
 function App() {
   const location = useLocation();
   return (
@@ -15,11 +18,10 @@ function App() {
               <Link
                 to="/"
                 className={`w-full flex items-center 
-        ${
-          location.pathname === "/"
-            ? "border-l-4 border-blue-700 font-semibold text-gray-800 hover:text-white"
-            : ""
-        }`}
+        ${location.pathname === "/"
+                    ? "border-l-4 border-blue-700 font-semibold text-gray-800 hover:text-white"
+                    : ""
+                  }`}
               >
                 🏠 Trang chủ
               </Link>
@@ -31,26 +33,59 @@ function App() {
               <Link
                 to="/drivers"
                 className={`w-full  flex items-center 
-        ${
-          location.pathname === "/drivers"
-            ? "border-l-4 border-blue-700 font-semibold text-gray-800 hover:text-white"
-            : ""
-        }`}
+        ${location.pathname === "/drivers"
+                    ? "border-l-4 border-blue-700 font-semibold text-gray-800 hover:text-white"
+                    : ""
+                  }`}
               >
                 👨‍✈️Tài xế
               </Link>
             </li>
+
+
             <li className="p-3 flex hover:bg-blue-500 hover:text-white items-center cursor-pointer">
-              🚌Xe buýt
+              <Link
+                to="/buses"
+                className={`w-full  flex items-center 
+        ${location.pathname === "/buses"
+                    ? "border-l-4 border-blue-700 font-semibold text-gray-800 hover:text-white"
+                    : ""
+                  }`}
+              >
+                🚌Xe buýt
+              </Link>
             </li>
+
             <li className="p-3 flex hover:bg-blue-500 hover:text-white items-center cursor-pointer">
               🗺️Tuyến đường
             </li>
+
+
             <li className="p-3 flex hover:bg-blue-500 hover:text-white items-center cursor-pointer">
-              🎓Học sinh
+              <Link
+                to="/students"
+                className={`w-full  flex items-center 
+        ${location.pathname === "/students"
+                    ? "border-l-4 border-blue-700 font-semibold text-gray-800 hover:text-white"
+                    : ""
+                  }`}
+              >
+                🎓Học sinh
+              </Link>
             </li>
+
             <li className="p-3 flex hover:bg-blue-500 hover:text-white items-center cursor-pointer">
-              👪Phụ huynh
+
+              <Link
+                to="/parents"
+                className={`w-full  flex items-center 
+        ${location.pathname === "/parents"
+                    ? "border-l-4 border-blue-700 font-semibold text-gray-800 hover:text-white"
+                    : ""
+                  }`}
+              >
+                👪Phụ huynh
+              </Link>
             </li>
             <li className="p-3 flex hover:bg-blue-500 hover:text-white items-center cursor-pointer">
               🔔Thông báo
@@ -86,6 +121,9 @@ function App() {
           <Route path="/" element={<h1>Chào mừng đến Trang chủ!</h1>} />
 
           <Route path="/drivers" element={<DriversPage />} />
+          <Route path="/students" element={<ManageStudent />} />
+          <Route path="/buses" element={<ManageBus />} />
+          <Route path="/parents" element={<ManageParent />} />
         </Routes>
       </div>
     </div>
